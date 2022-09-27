af82918bc1c64f6d7dd398f0d7579de4eb5f94d0
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2022-09-27 12:24:55 -0300

    Create SaladeAula.java

---
+++ SaladeAula.java
@@ -0,0 +1,47 @@
+class SaladeAula  {
+
+  private int computadores;
+  
+  private int numalunospresentes;
+
+  private boolean todospcsusados;
+
+  public SaladeAula() {
+
+    computadores = 26;
+    numalunospresentes = 0;
+    todospcsusados = false;
+
+  }
+
+  public void TodosPcsUsados()  {
+
+    if (numalunospresentes >= computadores) 
+      todospcsusados = true;
+      
+    else todospcsusados = false;
+    
+  }
+
+  public void VerificaPcsUsados () {
+
+    TodosPcsUsados();
+
+    if (todospcsusados == true) 
+
+      System.out.println ("Todos os Pcs estao sendo usados.");
+      
+    else 
+      
+      System.out.println ("Ainda ha pcs livres para uso.");
+    
+  }
+
+  public void numalunos() {
+
+    this.numalunospresentes = 23;
+    
+  }
+
+  
+}
