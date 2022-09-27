90f039e7ce217a26fa6e85cb0e8b919ae0799d7e
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2022-09-27 13:35:17 -0300

    Create TestSaladeAula.java

---
+++ TestSaladeAula.java
@@ -0,0 +1,16 @@
+class TestSaladeAula {  
+
+  public static void main(String args[]) { 
+    
+    SaladeAula S1, S2;
+    
+    S1 = new SaladeAula();
+    S1.VerificaPcsUsados();
+    
+    S2 = new SaladeAula();
+    S2.numalunos();
+    S2.VerificaPcsUsados();
+  
+  } 
+  
+}
