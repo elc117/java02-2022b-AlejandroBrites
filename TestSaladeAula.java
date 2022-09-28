aa979679e1cb2fbfb313261b47f6e7431c4ba9e5
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2022-09-27 22:22:51 -0300

    Update TestSaladeAula.java

--- TestSaladeAula.java
+++ TestSaladeAula.java
@@ -1,14 +1,23 @@
-class TestSaladeAula {  
+class TesteSaladeAula {  
 
   public static void main(String args[]) { 
     
     SaladeAula S1, S2;
-    
+
+    System.out.println ("Sala 01");
+
     S1 = new SaladeAula();
     S1.VerificaPcsUsados();
+    S1.EncheTurma();
+    S1.VerificaPcsUsados();
+
+    System.out.println ("\nSala 02");  
     
     S2 = new SaladeAula();
-    S2.numalunos();
+    S2.VerificaPcsUsados();
+    S2.EncheTurma();
+    S2.VerificaPcsUsados();
+    S2.EsvaziaTurma();
     S2.VerificaPcsUsados();
   
   } 
